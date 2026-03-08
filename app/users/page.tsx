6b9d2bfbd835c2efe01f5813db8e1ed99947612d
Pascal JK <82771457+PascalJK@users.noreply.github.com>
2026-03-08 10:10:07 +0200

    - learning importing plugins - using imported plugins
    - learned using a plugin called daisyui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -14,11 +14,22 @@ const UsersPage = async () => {
     <>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
