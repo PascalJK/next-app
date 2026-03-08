b52538625b73ed62bee46d526af7a3087cad2584
Pascal JK <82771457+PascalJK@users.noreply.github.com>
2026-03-08 09:08:45 +0200

    - dynamic and static rendering practice

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,22 +7,13 @@ interface User {
 }
 
 const UsersPage = async () => {
-  var result = await fetch("https://jsonplaceholder.typicode.com/users", {
-    // This will make it so data is not cached and fetched on every load.
-    cache: "no-store",
-
-    next: {
-      // Adding this makes it so data is refreshed from the backend every 10 seconds
-      revalidate: 10
-    },
-
-    // NOTE: This caching behaviour only works in the fetch function. It wont work in 3rd-party libs.
-  });
+  var result = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await result.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.username}</li>
